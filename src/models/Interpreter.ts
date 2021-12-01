0834c239ccc1cbef97b2d460b1fa2b9a2a7ed15c
PIG208 <359101898@qq.com>
2021-12-01 16:48:57 -0500

    Store program in ProgramState to implement loops

--- src/models/Interpreter.ts
+++ src/models/Interpreter.ts
@@ -1,11 +1,16 @@
 import { IOStream, read, write } from "./IOStream";
 
-type Instruction = "<" | ">" | "," | "." | "[" | "]" | "+" | "-";
+export type Instruction = "<" | ">" | "," | "." | "[" | "]" | "+" | "-";
 
-type ProgramState = {
+export type ProgramState = {
+  programCounter: number;
   dataPointer: number;
   memory: number[];
   jmpStack: number[];
+  program: Instruction[];
+
+  skipping: boolean;
+
   stdin: IOStream;
   stdout: IOStream;
 };
@@ -19,11 +24,15 @@ const readMemory = (state: ProgramState) => state.memory[state.dataPointer];
 const writeMemory = (state: ProgramState, data: number) =>
   (state.memory[state.dataPointer] = data);
 
-export const consume = (
-  instruction: Instruction,
-  state: ProgramState
-): ProgramState => {
+export const consume = (state: ProgramState): ProgramState => {
   let newState = copyState(state);
+  const instruction = fetchInstruction(newState);
+  let overridePc = false;
+
+  if (newState.skipping && instruction !== "]") {
+    newState.programCounter++;
+    return newState;
+  }
 
   switch (instruction) {
     case ">":
@@ -47,10 +56,28 @@ export const consume = (
       newState.stdout = write(newState.stdout, readMemory(newState));
       break;
     case "[":
+      if (readMemory(newState) !== 0) {
+        // We can jump back to this point
+        newState.jmpStack.push(newState.programCounter);
+      } else {
+        // Skip the entire loop body until we reaches the corresponding ]
+        newState.skipping = true;
+      }
       break;
     case "]":
+      if (readMemory(newState) === 0) {
+        // When the data pointer becomes 0, reset the stack and continue
+        newState.jmpStack.pop();
+      } else {
+        // Jump back to the top of the jmpStack
+        newState.programCounter =
+          newState.jmpStack[newState.jmpStack.length - 1];
+        overridePc = true;
+      }
       break;
   }
+
+  if (!overridePc) newState.programCounter++;
 
   return newState;
 };
