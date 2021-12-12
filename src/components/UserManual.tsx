f5a4caf7bab4e4be44a6d669829e0985736dbff0
PIG208 <359101898@qq.com>
2021-12-11 23:31:21 -0500

    Fix usermanual table

--- src/components/UserManual.tsx
+++ src/components/UserManual.tsx
@@ -6,48 +6,52 @@ const UserManual = () => {
       <h2>User Manual</h2>
       <p>A BF program has 8 different instructions</p>
       <table className="manual-table">
-        <tr>
-          <th>Instruction</th>
-          <th>Description</th>
-        </tr>
-        <tr>
-          <td>&gt;</td>
-          <td>Move the data pointer to the right</td>
-        </tr>
-        <tr>
-          <td>&lt;</td>
-          <td>Move the data pointer to the left</td>
-        </tr>
-        <tr>
-          <td>+</td>
-          <td>Increment the current memory cell</td>
-        </tr>
-        <tr>
-          <td>-</td>
-          <td>Decrement the current memory cell</td>
-        </tr>
-        <tr>
-          <td>,</td>
-          <td>Take one input from the stdin</td>
-        </tr>
-        <tr>
-          <td>.</td>
-          <td>Print the current memory (decoded as ASCII)</td>
-        </tr>
-        <tr>
-          <td>[</td>
-          <td>
-            Do nothing when the current memory cell is 0, otherwise jump to corresponding{" "}
-            <code>]</code>
-          </td>
-        </tr>
-        <tr>
-          <td>]</td>
-          <td>
-            Do nothing when the current memory cell is 0, otherwise jump to corresponding{" "}
-            <code>[</code>
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Instruction</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>&gt;</td>
+            <td>Move the data pointer to the right</td>
+          </tr>
+          <tr>
+            <td>&lt;</td>
+            <td>Move the data pointer to the left</td>
+          </tr>
+          <tr>
+            <td>+</td>
+            <td>Increment the current memory cell</td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>Decrement the current memory cell</td>
+          </tr>
+          <tr>
+            <td>,</td>
+            <td>Take one input from the stdin</td>
+          </tr>
+          <tr>
+            <td>.</td>
+            <td>Print the current memory (decoded as ASCII)</td>
+          </tr>
+          <tr>
+            <td>[</td>
+            <td>
+              Do nothing when the current memory cell is 0, otherwise jump to corresponding{" "}
+              <code>]</code>
+            </td>
+          </tr>
+          <tr>
+            <td>]</td>
+            <td>
+              Do nothing when the current memory cell is 0, otherwise jump to corresponding{" "}
+              <code>[</code>
+            </td>
+          </tr>
+        </tbody>
       </table>
       <p>
         Enter your program in the <span className="bordered-text">Editor</span>. If it is nonempty,
