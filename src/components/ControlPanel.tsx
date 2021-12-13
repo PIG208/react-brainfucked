c89f4ada39c4a34e2ca0a87933b2375301307150
PIG208 <359101898@qq.com>
2021-12-12 21:08:19 -0500

    Reformat control panel

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -11,88 +11,86 @@ export type ControlPanelProps = {
   dispatch: (action: BrainfuckAction) => void;
 };
 
-const ControlPanel = ({ programState, setCode, dispatch }: ControlPanelProps) => {
-  return (
-    <div>
-      <h2>
-        Control Panel{" "}
-        {programState.program.length === 0 && (
-          <span className="note">(enter any bf program to get started)</span>
-        )}
-      </h2>
-      <p>
-        Status:{" "}
-        {!isStarted(programState)
-          ? "not started"
-          : isEnded(programState)
-          ? "ended"
-          : programState.blocked
-          ? `${programState.blockType === "error" ? "error" : "blockedg"}${
-              programState.blockType === "breakpoint"
-                ? " (breakpoint)"
-                : programState.blockType === "io"
-                ? " (input required)"
-                : programState.blockType === "error"
-                ? ` (${programState.errorCode})`
-                : ""
-            }`
-          : "running"}
-      </p>
-      <ul className="panel">
-        <li>
-          <button
-            className="btn"
-            onClick={() => {
-              if (isPaused(programState)) dispatch({ type: "continue" });
-              dispatch({ type: "run" });
-            }}
-            disabled={
-              programState.program.length === 0 ||
-              isEnded(programState) ||
-              (programState.blocked && !isPaused(programState))
+const ControlPanel = ({ programState, setCode, dispatch }: ControlPanelProps) => (
+  <div>
+    <h2>
+      Control Panel{" "}
+      {programState.program.length === 0 && (
+        <span className="note">(enter any bf program to get started)</span>
+      )}
+    </h2>
+    <p>
+      Status:{" "}
+      {!isStarted(programState)
+        ? "not started"
+        : isEnded(programState)
+        ? "ended"
+        : programState.blocked
+        ? `${programState.blockType === "error" ? "error" : "blockedg"}${
+            programState.blockType === "breakpoint"
+              ? " (breakpoint)"
+              : programState.blockType === "io"
+              ? " (input required)"
+              : programState.blockType === "error"
+              ? ` (${programState.errorCode})`
+              : ""
+          }`
+        : "running"}
+    </p>
+    <ul className="panel">
+      <li>
+        <button
+          className="btn"
+          onClick={() => {
+            if (isPaused(programState)) dispatch({ type: "continue" });
+            dispatch({ type: "run" });
+          }}
+          disabled={
+            programState.program.length === 0 ||
+            isEnded(programState) ||
+            (programState.blocked && !isPaused(programState))
+          }
+        >
+          {isStarted(programState) ? "Continue" : "Run"}
+        </button>
+      </li>
+      <li>
+        <button
+          className="btn"
+          onClick={() => {
+            if (isPaused(programState)) dispatch({ type: "continue" });
+            else dispatch({ type: "next" });
+          }}
+          disabled={isEnded(programState) || (programState.blocked && !isPaused(programState))}
+        >
+          {!isStarted(programState) ? "Start" : "Step"}
+        </button>
+      </li>
+      <li>
+        <button
+          className="btn"
+          onClick={() => {
+            if (!isStarted(programState)) {
+              dispatch({ type: "reset-io" });
             }
-          >
-            {isStarted(programState) ? "Continue" : "Run"}
-          </button>
-        </li>
-        <li>
-          <button
-            className="btn"
-            onClick={() => {
-              if (isPaused(programState)) dispatch({ type: "continue" });
-              else dispatch({ type: "next" });
-            }}
-            disabled={isEnded(programState) || (programState.blocked && !isPaused(programState))}
-          >
-            {!isStarted(programState) ? "Start" : "Step"}
-          </button>
-        </li>
-        <li>
-          <button
-            className="btn"
-            onClick={() => {
-              if (!isStarted(programState)) {
-                dispatch({ type: "reset-io" });
-              }
-              dispatch({ type: "reset" });
-            }}
-            disabled={programState.program.length === 0}
-          >
-            Reset{" "}
-            {!isStarted(programState) &&
-            (programState.stdin.pointer > 0 || programState.stdout.pointer > 0)
-              ? "IO"
-              : ""}
-          </button>
-        </li>
-        <li>
-          <button className="btn" onClick={() => setCode(testHelloWorld.raw)}>
-            Hello World!
-          </button>
-        </li>
-      </ul>
-    </div>
-  );
-};
+            dispatch({ type: "reset" });
+          }}
+          disabled={programState.program.length === 0}
+        >
+          Reset{" "}
+          {!isStarted(programState) &&
+          (programState.stdin.pointer > 0 || programState.stdout.pointer > 0)
+            ? "IO"
+            : ""}
+        </button>
+      </li>
+      <li>
+        <button className="btn" onClick={() => setCode(testHelloWorld.raw)}>
+          Hello World!
+        </button>
+      </li>
+    </ul>
+  </div>
+);
 
 export default ControlPanel;
